a4f88f00e8fe55678a121506342db7488f25fadb
Anri <anri0806@hotmail.co.jp>
2023-05-22 16:12:25 -0400

    added hover states on button and form fields

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 addEventListener("DOMContentLoaded", () => {
   handleSubmit();
+  handleHoverEffect();
 });
 
 function handleSubmit() {
@@ -11,13 +12,26 @@ function handleSubmit() {
     let handle = document.getElementById("handle");
 
     if (name.value && email.value && handle.value) {
-      let background = document.getElementById("wrap")
-      background.removeAttribute("id")
-      background.setAttribute("id", "invisible")
+      let background = document.getElementById("wrap");
+      background.removeAttribute("id");
+      background.setAttribute("id", "invisible");
     }
 
     name.value = "";
     email.value = "";
     handle.value = "";
+  });
+}
+
+function handleHoverEffect() {
+
+  let button = document.getElementById("button");
+
+  button.addEventListener("mouseover", () => { 
+    button.innerHTML = `Submit <span style="color:green">✔<span>`
+  });
+
+  button.addEventListener("mouseout", () => { 
+    button.innerHTML = "Submit"
   });
 }
